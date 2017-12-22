0461ef92c1d226d40c01b1c7bcaa9789bf9f3743
harunnn <harun.rasyid@koinworks.com>
2017-12-22 16:18:32 +0700

    belajar validasi untuk operation, yap ini seperti konsep form object

--- app/concepts/forum_thread/operation.rb
+++ app/concepts/forum_thread/operation.rb
@@ -1,7 +1,35 @@
+# ------------------------------------------------
+# note : forumthread disini samasekali bukan model
+# forumthread disini hanya sekedar namespace saja
+# ------------------------------------------------
 class ForumThread < ActiveRecord::Base
   class Create < Trailblazer::Operation
-    def process(params = {})
-      @model = ForumThread.create(params[:forum_thread])
+    include Model
+
+    model ForumThread, :create # model semantik
+
+    # --------------------------------------------------
+    # note : isi blok di dalam contract merupakan sebuah
+    # validasi yang digunakan oleh model forum_thread.rb
+    # -------------------------------------------------
+    contract do
+      property :title
+      property :content
+
+      validates :title, presence: true, length: {in: 5..30}
+      validates :content, allow_blank: true, length: {in: 4..160}
+    end
+
+    def process(params = {forum_thread: {title: "", content: ""}})
+      # -------------------------------------------------------------------
+      # note : yuhuuu, meng-include Model sama saja seperti menginstruksi
+      # rails untuk menggunakan model dan operasinya, yap disini kita meng-
+      # gunakan operasi create dan tadaaaa~ trailblazer langsung tau model
+      # dan method apa yang ingin digunakan setelah proses validasi :p
+      # -------------------------------------------------------------------
+      validate(params[:forum_thread]) do |f|
+        f.save
+      end
     end
   end
 end
--- test/concepts/forum_thread/operation_test.rb
+++ test/concepts/forum_thread/operation_test.rb
@@ -13,4 +13,25 @@ class ForumThreadOperationTest < MiniTest::Spec
       forum_thread.content.must_equal "Halo nama saya harun"
     end
   end
+
+  describe "Can't create" do
+    it 'persist not valid' do
+      # --------------------------------------------------------------------
+      # note : penggunaan method run akan mengembalikan boolean hasil dari -
+      # validasi dan hasil operasinya ..
+      # --------------------------------------------------------------------
+      result, operation = ForumThread::Create.run(forum_thread: {title: "", content: ""})
+      result.must_equal false
+      # ----------------------------------------------------------
+      # note : penggunaan method model, digunakan untuk meng-akses
+      # record dari hasil operasi, dalam kasus ini method create
+      # ----------------------------------------------------------
+      operation.model.persisted?.must_equal false
+      # ---------------------------------------------------------
+      # note : method contract berguna untuk mengakses error yang
+      # didapat, yap kita mendefinisikan ulang di operation.rb
+      # ---------------------------------------------------------
+      operation.contract.errors.size.must_equal 2
+    end
+  end
 end
